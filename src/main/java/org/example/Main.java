ddc4588fdd1174f5a7fd934bd2d191262faa43f8
Corne Marais <cmarais@DVTL100760.local>
2022-12-22 20:34:33 +0200

    Test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.diver", "Users/cmarais/Downloads/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.navigate().to("https://rahulshettyacademy.com/");
+
+        driver.manage().window().maximize();
+
+
+
+
+
+
+    }
+}
